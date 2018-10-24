cdd0499136031128130610707b43c9a5312c1aae
Suhas Saheer Mohammad Sajad <Suhas.Sajad@emaratech.ae>
2018-10-24 13:31:49 +0400

    Spring Cache support implemented

--- src/main/java/com/suhas/service/lookup/LookupServiceApplication.java
+++ src/main/java/com/suhas/service/lookup/LookupServiceApplication.java
@@ -2,8 +2,10 @@ package com.suhas.service.lookup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LookupServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/suhas/service/lookup/service/impl/LookupServiceImpl.java
+++ src/main/java/com/suhas/service/lookup/service/impl/LookupServiceImpl.java
@@ -7,19 +7,29 @@ import com.suhas.service.lookup.service.LookupService;
 import com.suhas.service.lookup.utils.CommonUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
+@CacheConfig(cacheNames = "countryCache")
 public class LookupServiceImpl implements LookupService {
 
     @Autowired
     private CountryRepository countryRepository;
 
+    /**
+     * when method annotated with @Cacheable annotation,
+     * it will be executed only once for the given cachekey, until the cache expires or gets cleared.
+     * @return
+     */
     @Override
+    @Cacheable
     public List<CountryDTO> getAllCountries() {
         List<CountryDTO> countryDTOList = null;
+        simulateSlowService();
         Iterable<Country> iterableCountry = countryRepository.findAll();
         if (CommonUtils.isNotNull(iterableCountry)) {
             countryDTOList = new ArrayList<>();
@@ -38,5 +48,13 @@ public class LookupServiceImpl implements LookupService {
             });
         }
         return countryDTOList;
+    }
+
+    private void simulateSlowService() {
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
